2bfaa9f60f820b57ec2a6c2171ead4d46d2bb72c
PeytonWong <779409284@qq.com>
2019-07-01 11:03:28 +0800

    first commit

---
+++ success/DataExtract/Bag_path.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+import time
+
+TimeFormat = "%Y-%m-%d-%H-%M-%S"
+TimeLocal = time.localtime()
+
+# *******************************************
+
+car_follow_stop_and_go = '/home/dr/Work_Space/success/car_follow_stop_and_go.bag'
+car_follow_various_speed = '/home/dr/Work_Space/success/car_follow_various_speed.bag'
+cross_walk = '/home/dr/Work_Space/success/cross_walk.bag'
+cut_in = '/home/dr/Work_Space/success/cut_in.bag'
+lane_change = '/home/dr/Work_Space/success/lane_change.bag'
+meeting = '/home/dr/Work_Space/success/meeting.bag'
+overtake = '/home/dr/Work_Space/success/overtake.bag'
+pull_over = '/home/dr/Work_Space/success/pull_over.bag'
+pull_over_emergency = '/home/dr/Work_Space/success/pull_over_emergency.bag'
+road_block_corn = '/home/dr/Work_Space/success/road_block_corn.bag'
+road_block_fence = '/home/dr/Work_Space/success/road_block_fence.bag'
+speed_limit = '/home/dr/Work_Space/success/speed_limit.bag'
+stop_sign = '/home/dr/Work_Space/success/stop_sign.bag'
+take_over_button = '/home/dr/Work_Space/success/take_over_button.bag'
+take_over_lateral_steer_wheel = '/home/dr/Work_Space/success/take_over_lateral_steer_wheel.bag'
+take_over_longitudinal_brake_pedal = '/home/dr/Work_Space/success/take_over_longitudinal_brake_pedal.bag'
+xingneng_lane_detection = '/home/dr/Work_Space/success/xingneng_lane_detection.bag'
---
+++ success/DataExtract/ss.py
@@ -0,0 +1,5 @@
+for data in data14:
+    sheet1.write(i, 12, data)
+    # sheet1.write(i, 12, data*3.6/0.1)
+    i += 1
+i = 1
